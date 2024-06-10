7dedde272151ee9ac2b284f9f681d319aeef6c42
matteopoliti <matteo.politi17@gmail.com>
2024-06-10 11:19:28 +0200

    Feat: added header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
 import axios from "axios";
 import { store } from "./store";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
     components: {
-
+        AppHeader,
     },
     data() {
         return {
@@ -16,7 +17,9 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <AppHeader />
+</template>
 
 <style lang="scss">
 @use "./styles/general.scss";
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container-fluid">
+            <header
+                class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom">
+                <div class="col-md-3 mb-2 mb-md-0 d-flex align-items-center">
+                    <a href=""
+                        class="d-inline-flex link-body-emphasis text-decoration-none">
+
+                        <img src="../assets/img/logo_ticket.jpg" alt="Logo" class="img-fluid"
+                            style="height: 80px;">
+
+                    </a>
+                </div>
+            </header>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+    data () {
+    
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

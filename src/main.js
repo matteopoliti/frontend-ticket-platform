497b2b3e5603dc4f85490481fc88eb2a7269af73
matteopoliti <matteo.politi17@gmail.com>
2024-06-11 12:35:46 +0200

    Feat: added Font Awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import "@fortawesome/fontawesome-free/css/all.css"
+
 import { router } from "./router";
 
 createApp(App).use(router).mount('#app')

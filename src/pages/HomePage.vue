fb04339f9294d55e9d171e0b4250462ac5e93262
matteopoliti <matteo.politi17@gmail.com>
2024-06-11 13:14:00 +0200

    Feat: added button to change page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,8 +8,8 @@
                 <h4 class="fw-light">Alla ricerca di soluzioni? Sei nel posto giusto.</h4>
                 <p class="lead text-body-secondary">Il nostro portale di ricerca di ticket di supporto ti consente di accedere rapidamente alle risposte di cui hai bisogno. Esplora i ticket risolti e trova soluzioni già pronte ai tuoi problemi.</p>
                 <div class="d-flex justify-content-center">
-                    <a href="#" class="btn btn-primary my-2 ms-4">Inizia ora!</a>
-                    <router-link :to="{ name: 'ticket' }" class="btn btn-secondary my-2 ms-4">
+                    <a href="#" class="btn btn-outline-dark my-2 ms-4">Inizia ora!</a>
+                    <router-link :to="{ name: 'ticket' }" class="btn btn-outline-dark my-2 ms-4">
                         Cerca ticket
                     </router-link>
                 </div>
@@ -26,7 +26,7 @@
                         <div class="bg-secondary rounded-top d-flex justify-content-center">
                             <div class="d-flex flex-column align-items-center p-5">
                                 <i :class="item.category.icon" class="text-white fs-1 mb-3"></i>
-                                <h5 class="text-white">{{ item.category.name }}</h5>
+                                <h6 class="text-white">{{ item.category.name }}</h6>
 
                             </div>
                         </div>
@@ -46,6 +46,27 @@
             </div>
         </div>
     </div>
+    <nav aria-label="Page navigation example" class="mb-2 d-flex justify-content-center container">
+        <ul class="pagination pagination-sm flex-wrap text-dark">
+            <!-- Button Previous -->
+            <li class="page-item d-none d-sm-block">
+                <button class="page-link text-secondary-emphasis" :class="{ disabled: currentPage === 1 }" @click="
+                getTickets(currentPage - 1);
+                //moveToGrid();
+              ">
+                    Precedente
+                </button>
+            </li>
+            <li class="page-item d-none d-sm-block">
+                <button class="page-link text-secondary-emphasis" :class="{ disabled: currentPage === lastPage }" @click="
+                getTickets(currentPage + 1);
+                //moveToGrid();
+              ">
+                    Successivo
+                </button>
+            </li>
+        </ul>
+    </nav>
 
 </main>
 </template>
@@ -61,21 +82,29 @@ export default {
     data() {
 
         return {
-            arrayTickets: []
+            arrayTickets: [],
+            currentPage: "",
+            lastPage: "",
         }
     },
     methods: {
-        getTickets() {
+        getTickets(postApiPage) {
             axios
-                .get(`${store.apiBaseUrl}/api/tickets`)
+                .get(`${store.apiBaseUrl}/api/tickets`, {
+                    params: {
+                        page: postApiPage,
+                    },
+                })
                 .then(result => {
-                    this.arrayTickets = result.data.result.data
+                    this.arrayTickets = result.data.result.data;
+                    this.currentPage = result.data.result.current_page;
+                    this.lastPage = result.data.result.last_page;
                     console.log(this.arrayTickets)
                 })
         }
     },
     mounted() {
-        this.getTickets()
+        this.getTickets(1)
     },
 }
 </script>
